f3291387cbfa7710d346cf6e1e4ca7e10f2aa795
Alejandra Lopez <mlopezalejandra13@gmail.com>
2023-08-08 20:11:21 -0500

    suma

---
+++ script.js
@@ -0,0 +1,18 @@
+// Algoritmo para sumar dos números
+alert("Algoritmo para sumar dos números");
+
+// Paso a paso
+alert("Paso a paso:");
+var numero1 = prompt("1. Obtener el primer número a sumar");
+var numero2 = prompt("2. Obtener el segundo número a sumar");
+
+// Convertir cadenas a enteros usando parseInt
+numero1 = parseInt(numero1);
+numero2 = parseInt(numero2);
+
+var resultado = numero1 + numero2;
+
+// Mostrar el resultado usando la función alert
+alert("3. Adicionar la cantidad del segundo número al primer número");
+alert("4. Almacenar el resultado");
+alert("5. Mostrar el resultado: " + resultado);
